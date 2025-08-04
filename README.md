ffe41a082f93f93f46b28f7b28eb2befc0769885
Matthew Parkin <matthew.parkin@skybettingandgaming.com>
2025-08-04 21:21:08 +0100

    feat(CPT-4): implement questions and start  submission

--- README.md
+++ README.md
@@ -9,16 +9,14 @@ This is a short list of questions which our members can complete and will be giv
 First, run the development server:
 
 ```bash
-npm run dev
-# or
 yarn dev
-# or
-pnpm dev
-# or
-bun dev
 ```
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the Career Path Test.
+This will open the app with a user logged out message
+
+
+Open [http://localhost:3000?user=fudge](http://localhost:3000?user=fudge) to see the user fudges questions.
 
 
 ## Using Node Version Manager (nvm)
@@ -30,13 +28,23 @@ nvm install && nvm use
 ## Choices I made and limitations
 Start and approach: In the interest of time, I decided to add a skeleton of the app to get started quickly and achieve the brief without missing key components in the user journey.
 
-
 ## Core set up
 I created using vite template for React, TypeScript. Vite was chosen as it is a fast and modern build tool that supports React and TypeScript out of the box. I've used it in some projects and prefer to use it over next when not using server-side rendering.
 
 
 ## Testing approach
-Given more time I would have added more Unit Tests, but I wanted to focus on the user journey and the key components of the app. Tests were not specified in the brief. Realistically I would have aimed for at least a 70% coverage. I feel adding test would have provided assurance and helped demonstrate intent and functionality to future editors.
+Given more time I would have added Unit Tests, but I wanted to focus on the user journey and the key components of the app. Tests were not specified in the brief. Realistically I would have aimed for at least a 70% coverage. I feel adding test would have provided assurance and helped demonstrate intent and functionality to future editors. I would have used Jest and React Testing Library for unit tests.
 
 ## Css
-I should use a CSS framework such as Tailwind. I wanted to get closer to the original designs.
+I used pure CSS not wanting to delay set up realistically it probably would have been quicker to start with a CSS framework such as Tailwind. I wanted to get closer to the original designs. I mainly chose plain css to show that I understood core concepts.
+
+## Form
+I use a simple layout to display the questions and answers. The form is designed to be user-friendly and easy to navigate. I used a simple state management approach to handle the form data.
+ideally given more time I'd have implemented react hook form, but decided against it to save time on set up and focus on the user journey. Now looking back it seems slightly more complicated handling state. I think it would have made the code cleaner and easier to manage especially with the validation logic which was out of scope.
+
+## Query String
+For this solution, I opted for `URLSearchParams` as it provided a simple and straightforward approach without unnecessary complexity and is free out of the box.
+In previous apps I have built I mainly preferred using `qs` to handle query string data because I find it more readable and appriciate it offers better support for more complicated nested objects and arrays. 
+
+## Things I didn't hit in the brief:
+- Didn't get to displaying the zoom feature on the cards
--- src/components/TestWindow/index.tsx
+++ src/components/TestWindow/index.tsx
@@ -1,13 +1,51 @@
 import { useEffect, useState } from 'react';
-import { getQuestions } from '../helpers';
+import { getQuestions, submit } from '../helpers';
 import './index.css';
+import { Question } from '../Question';
 
 export const TestWindow = () => {
     const apiBaseURl = "https://fhc-api.onrender.com"
-
     const [user, setUser] = useState<string | null>(null);
     const [error, setError] = useState('');
+    const [questions, setQuestions] = useState<{ questions: { text: string; id: string }[] | null }>({ questions: null });
+    const [formState, setFormState] = useState({
+        answers: {} as Record<string, number>
+    });
 
+
+    const onSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+
+        if (!user) {
+            setError("User isn't logged in");
+            return;
+        }
+
+        submit(apiBaseURl, user, formState).then((response) => {
+            console.log("Submission Response:", response);
+        }).catch((error) => {
+            console.error("Submission Error:", error);
+            setError("An error occurred while submitting your answers.");
+        });
+    };
+
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+        console.log("Input Change:", { name, value });
+
+        setFormState({
+            ...formState,
+            answers: {
+                ...formState.answers,
+
+                // questionId: name, // Assuming questionId is the name of the input
+                // answer: parseInt(value),
+            },
+        });
+    };
 
     useEffect(() => {
         const queryString = new URLSearchParams(window.location.search);
@@ -15,34 +53,65 @@ export const TestWindow = () => {
 
         if (userParam) {
             setUser(userParam);
-            if (user) {
-                getQuestions(apiBaseURl, user).then((fetchedQuestions) => {
-                    console.log("Fetched Questions:", fetchedQuestions);
-                    return fetchedQuestions;
-                })
-            }
+            getQuestions(apiBaseURl, userParam).then((fetchedQuestions) => {
+                setQuestions(fetchedQuestions);
+            }).catch((error) => {
+                console.error("Error fetching questions:", error);
+                setError("An error occurred while fetching questions.");
+            });
         } else {
             setError("User not logged in");
         }
     }, []);
 
-    // Not the cleanest but it shows an error state if the user is not logged in. This wasn't part of 
-    // the ask but is helpful to the user journey.
+    // Not the cleanest but it shows an error state if the user is not logged in.
     if (error) {
         return <div className="error">{error}</div>;
     }
 
     return (
-        <section className="testWindow">
-            <div className="testWindow__Progress">
-                <p>Test Window</p>
-                <p>Progress: %</p>
-            </div>
+        <form onSubmit={onSubmit} >
+            <section className="testWindow">
+                <div className="testWindow__Progress">
+                    <p>Test Window</p>
+                    <p>Progress: %</p>
+                </div>
+                <div className="testWindow__Questions">
+                    {
+                        questions?.questions?.length ? (
+                            questions.questions.map((question, index) => {
+                                const { text, id } = question;
+                                return (
+                                    <div key={id}>
+                                        <Question
+                                            index={index}
+                                            text={text}
+                                        />
+                                        <div className="testWindow__AnswerScale--answerScale">
+                                            {[...Array(8)].map((_, value) => (
+                                                <input
+                                                    key={value}
+                                                    type="radio"
+                                                    name={id}
+                                                    value={value}
+                                                    onChange={handleInputChange}
+                                                />
+                                            ))}
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        ) : <div>Loading questions...</div>
+                    }
+                </div >
 
-            <div className="testWindow__Footer">
-                <p>To review your previous answers, scroll back before selecting "finish".</p>
-            </div>
+                <div className="testWindow__Footer">
+                    <p>To review your previous answers, scroll back before selecting "finish".</p>
+                </div>
 
-        </section>
+                <button type="submit">Submit</button>
+
+            </section>
+        </form>
     );
 };
